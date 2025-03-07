75b914c4b3b186c519adbbe0d7e6f77f93ef3d0b
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-03-07 22:28:03 +0530

    add Swagger Ui use as Client side

--- src/main/java/com/example/GreetingApp/Service/EmailService.java
+++ src/main/java/com/example/GreetingApp/Service/EmailService.java
@@ -1,12 +1,11 @@
 package com.example.GreetingApp.Service;
 
-
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -14,14 +13,18 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String content) {
+    public void sendEmail(String to, String subject, String body) {
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
             helper.setTo(to);
             helper.setSubject(subject);
-            helper.setText(content, true);
+            helper.setText(body, true);
+            helper.setFrom("abhiibaghel1011@gmail.com");
+
             mailSender.send(message);
+            System.out.println("Email sent successfully!");
         } catch (MessagingException e) {
             e.printStackTrace();
         }
